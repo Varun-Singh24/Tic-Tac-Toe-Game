e1cc21eeb58980e4d7d932151896dea0e5b2eaa9
Varun Pratap Singh <146599301+Varun-Singh24@users.noreply.github.com>
2024-04-24 16:28:10 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import TicTacToe from "./Components/Tic-Tac-Toe/TicTacToe";
+// import { useState } from "react";
+
+function App() {
+  return (
+    <div>
+      <TicTacToe />
+    </div>
+  );
+}
+export default App;
